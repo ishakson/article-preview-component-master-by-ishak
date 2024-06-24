b1bbd1b4b798d097ea30f7d9f78e1f71185503f8
ishak <ishaksylmz7@gmail.com>
2024-06-24 12:36:53 +0300

    main

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.getElementById('share-icon')
-const shareLinks = document.querySelector('.share-links');
-
-toggleBtn.addEventListener('click', function () {
-    shareLinks.parentElement.classList.toggle('active');
-})
